8c5839620c92c2ccd4dd769784abaa2a885a07a3
isadbdev <isabella.cazer13@gmail.com>
2026-07-30 19:55:34 -0300

    Adição de atributos na classe Jogador

--- src/main/java/com/mycompany/futplayers/FutPlayers.java
+++ src/main/java/com/mycompany/futplayers/FutPlayers.java
@@ -1,12 +1,13 @@
 package com.mycompany.futplayers;
 
 import com.mycompany.futplayers.model.Tecnico;
+import com.mycompany.futplayers.model.Jogador;
 
 public class FutPlayers {
     public static void main(String[] args) {
         Tecnico t = new Tecnico("Filipe Luís", "Flamengo");
-        Jogador j1 = new Jogador("Pedro Guilherme", 27, "Flamengo");
-        Jogador j2 = new Jogador("Arrascaeta", 30, "Flamengo");
+        Jogador j1 = new Jogador("Pedro Guilherme", 27, "Flamengo", 80.0, "Brasil");
+        Jogador j2 = new Jogador("Arrascaeta", 30, "Flamengo", 72.0, "Uruguai");
 
         Clube clube = new Clube("Flamengo", "BAP", "Filipe Luís");
         Confederacao conf = new Confederacao("CBF", "Ednaldo Rodrigues", "América do Sul");
@@ -21,8 +22,8 @@ public class FutPlayers {
         confService.listarConfederacoes();
 
         System.out.println("Técnico: " + t.getNome() + " Clube: " + t.getClube());
-        System.out.println("Jogador: " + j1.nome + " Idade: " + j1.idade);
-        System.out.println("Jogador: " + j2.nome + " Idade: " + j2.idade);
+        System.out.println("Jogador: " + j1.getNome() + " Idade: " + j1.getIdade() + " Peso: " + j1.getPeso() + " Nacionalidade: " + j1.getNacionalidade());
+        System.out.println("Jogador: " + j2.getNome() + " Idade: " + j2.getIdade() + " Peso: " + j2.getPeso() + " Nacionalidade: " + j2.getNacionalidade());
         System.out.println("Competição: " + comp.nome);
     }
 }
--- src/main/java/com/mycompany/futplayers/model/Jogador.java
+++ src/main/java/com/mycompany/futplayers/model/Jogador.java
@@ -1,13 +1,37 @@
-package com.mycompany.futplayers;
+package com.mycompany.futplayers.model;
 
 public class Jogador {
-    public String nome;
-    public int idade;
-    public String clube;
+    private String nome;
+    private int idade;
+    private String clube;
+    private double peso;
+    private String nacionalidade;
 
-    public Jogador(String n, int i, String c) {
-        nome = n;
-        idade = i;
-        clube = c;
+    public Jogador(String nome, int idade, String clube, double peso, String nacionalidade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.clube = clube;
+        this.peso = peso;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getClube() {
+        return clube;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
     }
 }
